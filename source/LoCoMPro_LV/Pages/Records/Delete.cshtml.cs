18dd0fe66a2b2754eb3ff62b6269032db46cba42
JamesLezcano <73266308+JamesLezcano@users.noreply.github.com>
2023-10-02 17:56:39 -0600

    Se crea CRUD para añadir botones de agregar registro

---
+++ source/LoCoMPro_LV/Pages/Records/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro_LV.Data;
+using LoCoMPro_LV.Models;
+
+namespace LoCoMPro_LV.Pages.Records
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly LoCoMPro_LV.Data.LoComproContext _context;
+
+        public DeleteModel(LoCoMPro_LV.Data.LoComproContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Record Record { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null || _context.Records == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records.FirstOrDefaultAsync(m => m.NameGenerator == id);
+
+            if (record == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Record = record;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null || _context.Records == null)
+            {
+                return NotFound();
+            }
+            var record = await _context.Records.FindAsync(id);
+
+            if (record != null)
+            {
+                Record = record;
+                _context.Records.Remove(Record);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ source/LoCoMPro_LV/Pages/Records/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro_LV.Data;
+using LoCoMPro_LV.Models;
+
+namespace LoCoMPro_LV.Pages.Records
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LoCoMPro_LV.Data.LoComproContext _context;
+
+        public IndexModel(LoCoMPro_LV.Data.LoComproContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Record> Record { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Records != null)
+            {
+                Record = await _context.Records
+                .Include(r => r.GeneratorUser)
+                .Include(r => r.Product)
+                .Include(r => r.Store).ToListAsync();
+            }
+        }
+    }
+}
