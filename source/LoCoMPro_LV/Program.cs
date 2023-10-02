18c06fc948a3cdf44d3df258ab4ac135f6a6a6e1
Gqbo <gabriel.gonzaf@gmail.com>
2023-10-02 17:51:07 -0600

    Se modifican restricciones y mensajes de error con respecto a la contraseña.

--- source/LoCoMPro_LV/Program.cs
+++ source/LoCoMPro_LV/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
 using LoCoMPro_LV.Data;
 using LoCoMPro_LV.Models;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,15 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<LoComproContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LoComproContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<LoComproContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredUniqueChars = 1;
+    options.Password.RequireDigit = true;
+}).AddEntityFrameworkStores<LoComproContext>();
 
 var app = builder.Build();
 
