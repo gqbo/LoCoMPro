f3afcb846967bb94742718fdaca879f5cdbdc4dd
Yordi Robles Siles <yordi.robles@ucr.ac.cr>
2023-10-03 17:04:08 -0600

    Se arregla los nombres de las columnas de la tabla de resultados, además, se bloquea el botón "buscar" si no hay nada escrito.

--- source/LoCoMPro_LV/Models/Record.cs
+++ source/LoCoMPro_LV/Models/Record.cs
@@ -6,26 +6,33 @@ namespace LoCoMPro_LV.Models
     {
         [Key]
         [Required]
+        
         [StringLength(256, MinimumLength = 2)]
         public string NameGenerator { get; set; }
 
+        [Display(Name = "Fecha de Registro")]
         [Required]
         public DateTime RecordDate { get; set; }
 
         [StringLength(256, MinimumLength = 2)]
         public string Description { get; set; }
 
+        [Display(Name = "Precio")]
         public float Price { get; set; }
 
+        [Display(Name = "Establecimiento")]
         [StringLength(100, MinimumLength = 2)]
         public string NameStore { get; set; }
 
+        [Display(Name = "Provincia")]
         [StringLength(50, MinimumLength = 2)]
         public  string NameProvince { get; set; }
 
+        [Display(Name = "Cantón")]
         [StringLength(50, MinimumLength = 2)]
         public  string NameCanton { get; set; }
 
+        [Display(Name = "Producto")]
         [StringLength(100, MinimumLength = 2)]
         public string NameProduct { get; set; }
 
