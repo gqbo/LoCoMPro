ad3d4b88d1e7e608d740a36956d7821794b9286d
Sebastián Rodríguez Tencio <sebastian.rodrigueztencio@ucr.ac.cr>
2023-10-02 20:43:05 -0600

    Se crean las operaciones CRUD del modelo Record.

---
+++ source/LoCoMPro_LV/Pages/Records/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro_LV.Data;
+using LoCoMPro_LV.Models;
+
+namespace LoCoMPro_LV.Pages.Records
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly LoCoMPro_LV.Data.LoComproContext _context;
+
+        public DetailsModel(LoCoMPro_LV.Data.LoComproContext context)
+        {
+            _context = context;
+        }
+
+        public Record Record { get; set; }
+
+        public async Task<IActionResult> OnGetDetails(string NameGenerator)
+        {
+            if (NameGenerator == null || _context.Records == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.Records.FirstOrDefaultAsync(m => m.NameGenerator == NameGenerator);
+
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            Record = record;
+            return Page();
+        }
+    }
+}
---
+++ source/LoCoMPro_LV/Pages/Records/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro_LV.Data;
+using LoCoMPro_LV.Models;
+
+namespace LoCoMPro_LV.Pages.Records
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LoCoMPro_LV.Data.LoComproContext _context;
+
+        public IndexModel(LoCoMPro_LV.Data.LoComproContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Record> Record { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            var recordsQuery = from m in _context.Records
+                               select m;
+
+            Record = await recordsQuery
+                .Include(r => r.GeneratorUser)
+                .Include(r => r.Product)
+                .Include(r => r.Store)
+                .Include(r => r.Store.Canton.Province)
+                .ToListAsync();
+        }
+    }
+}
