ed5139d63ed4d1da731c49241a1f2b0fc2f9d393
JamesLezcano <73266308+JamesLezcano@users.noreply.github.com>
2023-10-04 14:34:08 -0600

    Se implementa la funcionalidad de agregar registros sin control de provincias, cantones, locales, productos,

--- source/LoCoMPro_LV/Pages/Records/Create.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/Create.cshtml.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using LoCoMPro_LV.Data;
 using LoCoMPro_LV.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LoCoMPro_LV.Pages.Records
 {
@@ -21,23 +18,20 @@ namespace LoCoMPro_LV.Pages.Records
 
         public IActionResult OnGet()
         {
-        ViewData["NameGenerator"] = new SelectList(_context.GeneratorUsers, "UserName", "UserName");
-        ViewData["NameProduct"] = new SelectList(_context.Products, "NameProduct", "NameProduct");
-        ViewData["NameStore"] = new SelectList(_context.Stores, "NameStore", "NameStore");
             return Page();
         }
 
         [BindProperty]
         public Record Record { get; set; }
-        
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
+            
+            
 
             _context.Records.Add(Record);
             await _context.SaveChangesAsync();
