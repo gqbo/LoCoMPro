192ee4a30d29c218f716b73c76775c049cd84cfb
Sebastián Rodríguez Tencio <sebastian.rodrigueztencio@ucr.ac.cr>
2023-10-03 23:23:45 -0600

    Se crea la vista de la búsqueda avanzada y se añade la funcionalidad requerida para la realización de esta.

--- source/LoCoMPro_LV/Pages/Records/Index.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LoCoMPro_LV.Data;
 using LoCoMPro_LV.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LoCoMPro_LV.Pages.Records
 {
@@ -21,16 +22,67 @@ namespace LoCoMPro_LV.Pages.Records
 
         public IList<Record> Record { get; set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList Provinces { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchProvince { get; set; }
+
+        public SelectList Cantons { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchCanton { get; set; }
+
+        public SelectList Categories { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchCategory { get; set; }
+
         public async Task OnGetAsync()
         {
+            // Se cargan las listas de provincias, cantones y las diferentes categorías
+            var provinces = await _context.Provinces.ToListAsync();
+            var cantons = await _context.Cantons.ToListAsync();
+            var categories = await _context.Associated
+                                    .Select(a => a.NameCategory)
+                                    .Distinct()
+                                    .ToListAsync();
+
+            // Crear SelectList para provincias y cantones
+            Provinces = new SelectList(provinces, "NameProvince", "NameProvince");
+            Cantons = new SelectList(cantons, "NameCanton", "NameCanton");
+            Categories = new SelectList(categories);
+
+            // Resto de la lógica de búsqueda
             var recordsQuery = from m in _context.Records
                                select m;
+
+            // Se solicitan los distintos datos a la base de datos, dependiendo de lo buscado.
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                recordsQuery = recordsQuery.Where(s => s.NameProduct.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(SearchProvince))
+            {
+                recordsQuery = recordsQuery.Where(s => s.NameProvince == SearchProvince);
+            }
+
+            if (!string.IsNullOrEmpty(SearchCanton))
+            {
+                recordsQuery = recordsQuery.Where(s => s.NameCanton == SearchCanton);
+            }
+
+            if (!string.IsNullOrEmpty(SearchCategory))
+            {
+                recordsQuery = recordsQuery.Where(s => s.Product.Associated.Any(c => c.NameCategory == SearchCategory));
+            }
 
             Record = await recordsQuery
                 .Include(r => r.GeneratorUser)
                 .Include(r => r.Product)
                 .Include(r => r.Store)
                 .Include(r => r.Store.Canton.Province)
+                .Include(r => r.Product.Associated)
                 .ToListAsync();
         }
     }
