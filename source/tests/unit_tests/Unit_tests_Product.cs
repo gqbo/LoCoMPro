037b81da02443060a4da7a4cc35a40d8bdc4a98c
Yordi Robles Siles <yordi.robles@ucr.ac.cr>
2023-10-08 19:33:32 -0600

    Se agregan las pruebas unitarias relacionadas al modelo de "Product".

---
+++ source/tests/unit_tests/Unit_tests_Product.cs
@@ -0,0 +1,81 @@
+using LoCoMPro_LV.Areas.Identity.Pages.Account;
+using LoCoMPro_LV.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitTests_InputModel
+{
+    [TestClass]
+    public class UnitTests_InputModel
+    {
+        // Test by Yordi Robles Siles - C06557
+        [TestMethod]
+        public void Product_NameProduct_InvalidMinimunLength()
+        {
+            var productModel = new Product { NameProduct = "a" };
+            var context = new ValidationContext(productModel);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(productModel, context, results, true);
+
+            Assert.IsFalse(isValid);
+            Assert.IsTrue(results.Any(r => r.MemberNames.Contains("NameProduct")), "El nombre del producto debe ser inválido debido a que incumple la cantidad mínima de caracteres.");
+        }
+
+        // Test by Yordi Robles Siles - C06557
+        [TestMethod]
+        public void Product_NameProduct_InvalidMaximumLength()
+        {
+            var productModel = new Product { NameProduct = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" };
+            var context = new ValidationContext(productModel);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(productModel, context, results, true);
+
+            Assert.IsFalse(isValid);
+            Assert.IsTrue(results.Any(r => r.MemberNames.Contains("NameProduct")), "El nombre del producto debe ser inválido debido a que incumple la cantidad máxima de caracteres.");
+        }
+
+        // Test by Yordi Robles Siles - C06557
+        [TestMethod]
+        public void Product_NameProduct_Required()
+        {
+            var productModel = new Product { };
+            var context = new ValidationContext(productModel);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(productModel, context, results, true);
+
+            Assert.IsFalse(isValid);
+            Assert.IsTrue(results.Any(r => r.MemberNames.Contains("NameProduct")), "El nombre del producto debe ser invalido debido a que es obligatorio");
+        }
+
+        // Test by Yordi Robles Siles - C06557
+        [TestMethod]
+        public void Product_NameProduct_InvalidRegex()
+        {
+            var productModel = new Product { NameProduct = "Arroz12" };
+            var context = new ValidationContext(productModel);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(productModel, context, results, true);
+
+            Assert.IsFalse(isValid);
+            Assert.IsTrue(results.Any(r => r.MemberNames.Contains("NameProduct")), "El nombre del producto debe ser invalido debido a que contiene números");
+        }
+
+        // Test by Yordi Robles Siles - C06557
+        [TestMethod]
+        public void Product_NameProduct_ValidRegex()
+        {
+            var productModel = new Product { NameProduct = "Pc Gamer" };
+            var context = new ValidationContext(productModel);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(productModel, context, results, true);
+
+            Assert.IsTrue(isValid);
+           
+        }
+    }
+}
