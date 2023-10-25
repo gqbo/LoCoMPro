319cad2f731fd78f356de372364664960678a196
Sebastián Rodríguez Tencio <sebastian.rodrigueztencio@ucr.ac.cr>
2023-10-25 14:27:00 -0600

    Se añade la paginación a los detalles de un registro.

--- source/LoCoMPro_LV/Pages/Records/Details.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/Details.cshtml.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using LoCoMPro_LV.Data;
 using LoCoMPro_LV.Models;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Configuration;
+using LoCoMPro_LV.Utils;
+using Microsoft.Data.SqlClient;
 
 namespace LoCoMPro_LV.Pages.Records
 {
@@ -19,21 +22,25 @@ namespace LoCoMPro_LV.Pages.Records
         /// <summary>
         /// Contexto de la base de datos de LoCoMPro.
         /// </summary>
-        private readonly LoCoMPro_LV.Data.LoComproContext _context;
+        private readonly LoComproContext _context;
+
+        private readonly IConfiguration Configuration;
 
         /// <summary>
         /// Constructor de la clase DetailsModel.
         /// </summary>
         /// <param name="context">Contexto de la base de datos de LoCoMPro.</param>
-        public DetailsModel(LoCoMPro_LV.Data.LoComproContext context)
+        public DetailsModel(LoComproContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         /// <summary>
         /// Lista de tipo "Record", que almacena los registros correspondientes al producto que se selecciono para ver en detalle.
         /// </summary>
-        public IList<Record> Record { get; set; } = default!;
+        public PaginatedList<Record> Records { get; set; }
+        /*public IList<Record> Record { get; set; } = default!;*/
 
         /// <summary>
         /// Nombre del usuario generador del registro seleccionado en la pantalla index, que se utiliza para buscar todos los registros relacionados.
@@ -52,9 +59,8 @@ namespace LoCoMPro_LV.Pages.Records
         /// utiliza el nombre del generador y la fecha de realización del registro más reciente del producto para realizar la consulta por todos
         /// de todos los registros con ese producto en esa tienda en específico.
         /// </summary>
-        public async Task<IActionResult> OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(int? pageIndex)
         {
-
             var FirstRecord = await _context.Records
                 .FirstOrDefaultAsync(m => m.NameGenerator == NameGenerator && m.RecordDate == RecordDate);
 
@@ -62,7 +68,6 @@ namespace LoCoMPro_LV.Pages.Records
 
             if (FirstRecord != null)
             {
-
                 var allRecords = from m in _context.Records
                                  where m.NameProduct.Contains(FirstRecord.NameProduct) &&
                                        m.NameStore.Contains(FirstRecord.NameStore) &&
@@ -71,12 +76,15 @@ namespace LoCoMPro_LV.Pages.Records
                                  orderby m.RecordDate descending
                                  select m;
 
-                Record = await allRecords
-                    .ToListAsync();
+                var totalCount = await allRecords.CountAsync(); // Contiene el número total de registros.
+
+                var pageSize = Configuration.GetValue("PageSize", 10);
+                Records = await PaginatedList<Record>.CreateAsync(
+                    allRecords, pageIndex ?? 1, pageSize);
             }
             else
             {
-                Record = new List<Record>();
+                Records = new PaginatedList<Record>(new List<Record>(), 0, 1, 10);
             }
 
             return Page();
