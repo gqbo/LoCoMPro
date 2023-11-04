9bde7421f6de7f2d13c15bdf016b088b1368b8d1
JamesLezcano <73266308+JamesLezcano@users.noreply.github.com>
2023-11-03 19:42:39 -0600

    Se agregan comentarios y descripción de los diferentes metodos.

--- source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
@@ -6,17 +6,29 @@ using LoCoMPro_LV.Data;
 
 namespace LoCoMPro_LV.Pages.Records
 {
+    /// <summary>
+    /// Página Create de Records para la creación de nuevos registros registros.
+    /// </summary>
     public class MyRecordsModel : PageModel
-    {   
+    {
+        /// <summary>
+        /// Contexto de la base de datos de LoCoMPro.
+        /// </summary>
         private readonly LoComproContext _context;
         public MyRecordsModel(LoComproContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Enlazar los valores de las propiedades en un objeto con los datos provenientes de una solicitud HTTP.
+        /// </summary>
         [BindProperty]
         public List<Record> Records { get; set; }
 
+        /// <summary>
+        /// Método invocado cuando se realiza una solicitud GET para mostrar los registros de un usuario.
+        /// </summary>
         public async Task OnGetAsync()
         {
             string authenticatedUserName = User.Identity.Name;
