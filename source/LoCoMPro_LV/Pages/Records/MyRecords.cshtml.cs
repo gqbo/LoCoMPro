2152a89dff9a11161686556618b244206f2c23fd
JamesLezcano <73266308+JamesLezcano@users.noreply.github.com>
2023-11-03 19:41:53 -0600

    Se implementa funcionalidad de mis aportes

---
+++ source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LoCoMPro_LV.Models;
+using LoCoMPro_LV.Data;
+
+namespace LoCoMPro_LV.Pages.Records
+{
+    public class MyRecordsModel : PageModel
+    {   
+        private readonly LoComproContext _context;
+        public MyRecordsModel(LoComproContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public List<Record> Records { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            string authenticatedUserName = User.Identity.Name;
+            Records = await _context.Records
+                .Include(r => r.Store)
+                .Where(r => r.NameGenerator == authenticatedUserName)
+                .ToListAsync();
+        }
+    }
+}
