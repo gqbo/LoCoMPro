7819cbbcc163326846e83aee9fc5ab1857324151
Sebastián Rodríguez Tencio <sebastian.rodrigueztencio@ucr.ac.cr>
2023-11-05 14:41:43 -0600

    Se realiza el ordenamiento en el cliente y se muestra una flecha que indica como se esta realizando el ordenamiento.

--- source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
@@ -27,57 +27,15 @@ namespace LoCoMPro_LV.Pages.Records
         public List<Record> Records { get; set; }
 
         /// <summary>
-        /// String que permite elegir entorno a que atributo se ordenara.
+        /// Método invocado cuando se realiza una solicitud GET para mostrar los registros de un usuario.
         /// </summary>
-        public string eleccion { get; set; }
-
-        /// <summary>
-        /// String que permite elegir el orden.
-        /// </summary>
-        public string sortOrder { get; set; }
-
-        /// <summary>
-        /// Permite mostrar los resultados de Mis Aportes en la vista del mismo nombre. 
-        /// </summary>
-        public async Task OnGet(string sortOrder, string eleccion)
+        public async Task OnGetAsync()
         {
-            this.eleccion = eleccion;
-            this.sortOrder = sortOrder;
             string authenticatedUserName = User.Identity.Name;
-
-            IQueryable<Record> query = _context.Records.Include(r => r.Store).Where(r => r.NameGenerator == authenticatedUserName);
-
-            if (eleccion == "fecha")
-            {
-                query = ApplyDateSorting(query, sortOrder);
-            }
-            else if (eleccion == "precio")
-            {
-                query = ApplyPriceSorting(query, sortOrder);
-            }
-            Records = await query.ToListAsync();
-        }
-
-        /// <summary> 
-        /// Permite realizar un ordenamiento por medio de la fecha ya sea en forma ascendente o descendente.
-        /// </summary>
-        private IQueryable<Record> ApplyDateSorting(IQueryable<Record> query, string sortOrder)
-        {
-            bool isDateDescending = string.Equals(sortOrder, "Date_desc", StringComparison.OrdinalIgnoreCase);
-            query = isDateDescending ? query.OrderByDescending(r => r.RecordDate) : query.OrderBy(r => r.RecordDate);
-            ViewData["DateSort"] = isDateDescending ? "Date" : "Date_desc";
-            return query;
-        }
-
-        /// <summary> 
-        /// Permite realizar un ordenamiento por medio del precio ya sea en forma ascendente o descendente.
-        /// </summary>
-        private IQueryable<Record> ApplyPriceSorting(IQueryable<Record> query, string sortOrder)
-        {
-            bool isPriceDescending = string.Equals(sortOrder, "Price_desc", StringComparison.OrdinalIgnoreCase);
-            query = isPriceDescending ? query.OrderByDescending(r => r.Price) : query.OrderBy(r => r.Price);
-            ViewData["PriceSort"] = isPriceDescending ? "Price" : "Price_desc";
-            return query;
+            Records = await _context.Records
+                .Include(r => r.Store)
+                .Where(r => r.NameGenerator == authenticatedUserName)
+                .ToListAsync();
         }
     }
 }
