50759de3b13ce0c3d2f87d0bcfe724d098bce659
Gqbo <gabriel.gonzaf@gmail.com>
2023-11-04 19:18:03 -0600

    Se agrega gestión de cuenta para el correo electrónico de un usuario.

---
+++ source/LoCoMPro_LV/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -0,0 +1,120 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using LoCoMPro_LV.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace LoCoMPro_LV.Areas.Identity.Pages.Account.Manage
+{
+    public class EmailModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+
+        public EmailModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            [EmailAddress]
+            [Display(Name = "Nuevo correo electrónico")]
+            public string NewEmail { get; set; }
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var email = await _userManager.GetEmailAsync(user);
+            Email = email;
+
+            Input = new InputModel
+            {
+                NewEmail = email,
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var AuthenticatedUserName = User.Identity.Name;
+            var user = await _userManager.FindByNameAsync(AuthenticatedUserName);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with UserName: '{AuthenticatedUserName}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostChangeEmailAsync()
+        {
+            var AuthenticatedUserName = User.Identity.Name;
+            var user = await _userManager.FindByNameAsync(AuthenticatedUserName);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with UserName: '{AuthenticatedUserName}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var email = await _userManager.GetEmailAsync(user);
+            if (Input.NewEmail != email)
+            {
+                var result = await _userManager.SetEmailAsync(user, Input.NewEmail);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.UpdateAsync(user);
+                    await _signInManager.RefreshSignInAsync(user);
+                    StatusMessage = "Se ha actualizado el correo electrónico";
+                }
+            }
+            return RedirectToPage();
+        }
+    }
+}
