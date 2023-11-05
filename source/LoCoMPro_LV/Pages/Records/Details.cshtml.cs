12ff18af896e0bc1229f111eeca1556899f3b114
Sebastián Rodríguez Tencio <sebastian.rodrigueztencio@ucr.ac.cr>
2023-11-05 13:58:10 -0600

    Se elimina código muerto en el encabezado de Details.cshtml.cs de Reports.

--- source/LoCoMPro_LV/Pages/Records/Details.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/Details.cshtml.cs
@@ -1,12 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LoCoMPro_LV.Data;
