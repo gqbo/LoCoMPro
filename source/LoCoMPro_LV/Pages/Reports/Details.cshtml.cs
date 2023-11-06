d6797dc1ca4a48fe5396e983558624ffdab38465
CristopherH11 <cristo1206hc@gmail.com>
2023-11-05 20:05:20 -0600

    Se genera la vista para los detalles de los reportes donde se podran aceptar o rechazar.

--- source/LoCoMPro_LV/Pages/Reports/Details.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Reports/Details.cshtml.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LoCoMPro_LV.Data;
 using LoCoMPro_LV.Models;
+using LoCoMPro_LV.Pages.Records;
+using LoCoMPro_LV.Utils;
+using System.Data.SqlClient;
 
 namespace LoCoMPro_LV.Pages.Reports
 {
@@ -14,30 +17,139 @@ namespace LoCoMPro_LV.Pages.Reports
     {
         private readonly LoCoMPro_LV.Data.LoComproContext _context;
 
-        public DetailsModel(LoCoMPro_LV.Data.LoComproContext context)
+        /// <summary>
+        /// Se utiliza para acceder a las utilidades de la base de datos.
+        /// </summary>
+        private readonly DatabaseUtils _databaseUtils;
+
+        public DetailsModel(LoComproContext context, DatabaseUtils databaseUtils)
         {
             _context = context;
+            _databaseUtils = databaseUtils;
         }
 
-      public Report Report { get; set; }
+        /// <summary>
+        /// Lista de tipo "RecordStoreReportModel", que almacena los registros correspondientes al producto que se selecciono para ver en detalle, con su respectiva tienda.
+        /// </summary>
+        public RecordStoreReportModel recordStoreReports { get; set; } = default!;
 
-        public async Task<IActionResult> OnGetAsync(string id)
+        /// <summary>
+        /// Nombre del usuario generador del registro seleccionado en la pantalla index, que se utiliza para buscar todos los registros relacionados.
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public string NameGenerator { get; set; }
+
+        /// <summary>
+        /// Fecha en la que se realizó el registro seleccionado en la pantalla index, que se utiliza para buscar todos los registros relacionados.
+        /// </summary>
+        [BindProperty(SupportsGet = true)]
+        public DateTime RecordDate { get; set; }
+
+        /// <summary>
+        /// Método utilizado cuando en la pantalla de resultados de la búsqueda se selecciona un producto para abrir el detalle. Este método
+        /// utiliza el nombre del generador y la fecha de realización del registro más reciente del producto para realizar la consulta por todos
+        /// de todos los registros con ese producto en esa tienda en específico. Además se saca el promedio de estrellas para cada registro asociado
+        /// a un producto.
+        /// </summary>
+        public async Task<IActionResult> OnGetAsync()
         {
-            if (id == null || _context.Reports == null)
+            var query = await (from r in _context.Records
+                               join s in _context.Stores on new { r.Latitude, r.Longitude } equals new { s.Latitude, s.Longitude }
+                               where r.NameGenerator == NameGenerator && r.RecordDate == RecordDate
+                               select new
+                               {
+                                   Record = r,
+                                   Store = s
+                               })
+                 .FirstOrDefaultAsync();
+
+            RecordStoreReportModel temp = new RecordStoreReportModel
             {
-                return NotFound();
+                Record = query.Record,
+                Store = query.Store,
+
+                Reports = await GetReportsForRecordAsync(NameGenerator, RecordDate),
+
+                recordValoration = GetAverageRating(NameGenerator, RecordDate),
+
+                generatorValoration = GetUserRating(NameGenerator)
+            };
+
+            temp.reporterValorations = new List<int>();
+
+            foreach (Report report in temp.Reports)
+            {
+                int rating = GetUserRating(report.NameReporter);
+                temp.reporterValorations.Add(rating);
             }
 
-            var report = await _context.Reports.FirstOrDefaultAsync(m => m.NameGenerator == id);
-            if (report == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Report = report;
-            }
+            recordStoreReports = temp;
+
             return Page();
+        }
+
+        /// <summary>
+        /// Este metodo busca los reportes asociados a un registro
+        /// </summary>
+        /// <param name="nameGenerator">Es el nombre del usuario que genero el registro al cual se le busca los reportes</param>
+        /// <param name="recordDate">Es la fecha en la cual se genero el registro al cual se le busca los reportes</param>
+        private async Task<List<Report>> GetReportsForRecordAsync(string nameGenerator, DateTime recordDate)
+        {
+            var new_reports = from reports in _context.Reports
+                              where reports.NameGenerator == nameGenerator &&
+                                    reports.RecordDate == recordDate
+                              select reports;
+
+            return await new_reports.ToListAsync();
+        }
+
+        /// <summary>
+        /// Método utilizado para obtener el promedio de las valoraciones de estrellas de un registro en específico utilizando
+        /// una función escalar creada en la base de datos.
+        /// <param name="nameGenerator">Nombre del generador de un registro utilizado para utilizarlo como parámetro en la función escalar</param>
+        /// <param name="recordDate">Fecha de un registro utilizado para utilizarlo como parámetro en la función escalar</param>
+        /// </summary>
+        private int GetAverageRating(string nameGenerator, DateTime recordDate)
+        {
+            string connectionString = _databaseUtils.GetConnectionString();
+            string sqlQuery = "SELECT dbo.GetStarsAverage(@NameGenerator, @RecordDate)";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@NameGenerator", nameGenerator),
+                new SqlParameter("@RecordDate", recordDate)
+            };
+            int averageRating = DatabaseUtils.ExecuteScalar<int>(connectionString, sqlQuery, parameters);
+            return averageRating;
+        }
+
+        private int GetUserRating(string nameGenerator)
+        {
+            string connectionString = _databaseUtils.GetConnectionString();
+            string sqlQuery = "SELECT dbo.GetUserRating(@NameGenerator)";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@NameGenerator", nameGenerator)
+            };
+            int userRating = DatabaseUtils.ExecuteScalar<int>(connectionString, sqlQuery, parameters);
+            return userRating;
+        }
+
+        /// <summary>
+        /// Este metodo utiliza la informacion recopilada en la vista para crear el reporte y enviarlo a la base de datos.
+        /// </summary>
+        public async Task<IActionResult> OnPostAsync(string action, int customNumber)
+        {
+            if (action == "accept")
+            {
+                
+            }
+            else if (action == "reject")
+            {
+                
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToPage("../Index");
         }
     }
 }
