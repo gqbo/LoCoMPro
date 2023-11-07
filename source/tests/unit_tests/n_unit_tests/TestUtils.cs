af6014ed4b818269c6fc395504b0b1b6c6a66718
Yordi Robles Siles <yordi.robles@ucr.ac.cr>
2023-11-06 20:59:16 -0600

    Se realizan algunas pruebas unitarias relacionadas a la creación de una tienda y sus respectivos métodos auxiliares.

--- source/tests/unit_tests/n_unit_tests/TestUtils.cs
+++ source/tests/unit_tests/n_unit_tests/TestUtils.cs
@@ -8,10 +8,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using LoCoMPro_LV.Areas.Identity.Pages.Account;
+using LoCoMPro_LV.Pages.Stores;
+
 public class TestUtils
 {
     protected UserManager<ApplicationUser> UserManager { get; set; }
-
     public TestUtils()
     {
         var userStore = new Mock<IUserStore<ApplicationUser>>();
@@ -65,94 +66,10 @@ public class TestUtils
         {
             case "register_page":
                 return new RegisterModel(mockUserManager.Object, mockUserStore.Object, mockSignInManager.Object, mockLogger.Object, dbContext);
-
+            case "createStore_page":
+                return new CreateStoreModel(dbContext);
             default:
                 return new LoCoMPro_LV.Pages.IndexModel(dbContext);
         }
-    }
-
-    protected List<Record> CreateRegisters()
-    {
-        var user = new ApplicationUser
-        {
-            UserName = "anne",
-            FirstName = "Anne",
-            LastName = "Hathaway",
-            Latitude = 10.009,
-            Longitude = -84.1211,
-            NameProvince = "Heredia",
-            NameCanton = "Barva",
-            NormalizedUserName = "ANNE",
-            Email = "anne@gmail.com",
-            NormalizedEmail = "ANNE@GMAIL.COM",
-            EmailConfirmed = false,
-            PasswordHash = "AQAAAAIAAYagAAAAEJmsnmp+Rm7C6VMHu1s21eBFFButNJUpJ6E6yV5OnERn6c2Hv7KutoQrAaUPyez1lQ==",
-            SecurityStamp = "5NFRN2JU2Z7T7WPCJZ4LY4TA45YHLSXW",
-            ConcurrencyStamp = "cd77e5d0-740a-43f8-9ad4-3d8633ec6d01",
-            LockoutEnabled = true,
-            AccessFailedCount = 0
-        };
-
-        var generatorUser = new GeneratorUser
-        {
-            UserName = "anne",
-            ApplicationUser = user
-        };
-
-        var product = new Product
-        {
-            NameProduct = "Apple Iphone 11 64gb"
-        };
-
-        var province = new Province
-        {
-            NameProvince = "San José"
-        };
-
-        var canton = new Canton
-        {
-            NameCanton = "Tibás",
-            Province = province,
-            NameProvince = "San José"
-        };
-
-        var store = new Store
-        {
-            NameStore = "Ishop",
-            Canton = canton,
-            NameProvince = "San José",
-            NameCanton = "Tibás",
-            Latitude = 9.9516,
-            Longitude = -84.0990
-        };
-
-        Random random = new Random();
-
-        var result = new List<Record>();
-
-        for (int i = 0; i < 5; ++i)
-        {   
-            int year = random.Next(2022, 2024); 
-            int month = random.Next(1, 13); 
-            int day = random.Next(1, 29); 
-
-            DateTime randomDate = new DateTime(year, month, day);
-
-            result.Add(new Record()
-            {
-                NameGenerator = "anne",
-                GeneratorUser = generatorUser,
-                RecordDate = randomDate,
-                Price = random.Next(743, 1369),
-                NameStore = "Ishop",
-                Latitude = 9.9516,
-                Longitude = -84.0990,
-                NameProduct = "Apple Iphone 11 64gb",
-                Store = store,
-                Product = product,
-                Description = "Test comment"
-            });
-        }
-        return result;
     }
 }
