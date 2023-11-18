5e515ba0c2a36128dfea129591dba28c3128b8b2
CristopherH11 <cristo1206hc@gmail.com>
2023-11-17 18:51:13 -0600

    Se agregan los modelos relacionados al trabajo con las listas, y los productos en listas.

---
+++ source/LoCoMPro_LV/Models/Listed.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoCoMPro_LV.Models
+{
+    public class Listed
+    {
+        [Required(ErrorMessage = "El nombre de la lista es obligatorio.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "El nombre del establecimiento debe tener entre 2 y 50 caracteres.")]
+        [RegularExpression(@"^(?=.*[a-zA-ZáéíóúÁÉÍÓÚ])[\w\s,./\-()%:#áéíóúÁÉÍÓÚ]+$",
+            ErrorMessage = "El nombre del establecimiento debe contener al menos una letra (mayúscula o minúscula), además de números, espacios y caracteres especiales básicos, así como letras acentuadas.")]
+        public string NameList { get; set; }
+
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
+        [StringLength(50, MinimumLength = 2)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "El nombre del producto debe tener entre 2 y 50 caracteres.")]
+        [RegularExpression(@"^(?=.*[a-zA-ZáéíóúÁÉÍÓÚ])[\w\s,./\-()%:#áéíóúÁÉÍÓÚ]+$",
+            ErrorMessage = "El nombre del producto debe contener al menos una letra (mayúscula o minúscula), además de números, espacios y caracteres especiales básicos, así como letras acentuadas.")]
+        public string NameProduct { get; set; }
+
+        public Product Product { get; set; }
+
+        public List List { get; set; }
+    }
+}
