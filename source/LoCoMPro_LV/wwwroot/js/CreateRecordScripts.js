d4aca930814cac496d75f647ced8f950e941f3c3
Yordi Robles Siles <yordi.robles@ucr.ac.cr>
2023-11-21 12:39:34 -0600

    Se modifica la visualización de las imágenes en la pantalla del agregado del registro.

--- source/LoCoMPro_LV/wwwroot/js/CreateRecordScripts.js
+++ source/LoCoMPro_LV/wwwroot/js/CreateRecordScripts.js
@@ -1,47 +1,43 @@
-
-
-function handleImageDelete(index) {
-    var container = document.getElementById("imagePreviewContainer");
-    var images = container.getElementsByClassName("img-preview-container");
-
-    container.removeChild(images[index]);
-    var input = document.getElementById("fileInput");
-    var files = input.files;
-    var newFiles = Array.from(files);
-    newFiles.splice(index, 1);
-    input.files = newFiles;
-}
-
-function previewImages(event) {
+// Crea una previsualización de las imágenes.
+function handleFileSelection(event) {
     var input = event.target;
     var container = document.getElementById("imagePreviewContainer");
-    console.log(container);
-    var files = input.files;
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        if (file.type.startsWith("image/")) {
-            var reader = new FileReader();
-            reader.onload = (function (file, index) {
-                return function (e) {
-                    var containerItem = document.createElement("div");
-                    containerItem.className = "img-preview-container";
+    container.innerHTML = "";
 
+    var maxFiles = 3;
+
+    if (input.files.length > maxFiles) {
+        document.getElementById("fileCountError").textContent = "Solo puedes seleccionar hasta 3 archivos";
+        input.value = "";
+        return;
+    } else {
+        document.getElementById("fileCountError").textContent = "";
+    }
+
+    // Mostrar previsualización para cada archivo
+    for (var i = 0; i < input.files.length; i++) {
+        var file = input.files[i];
+
+        if (file.type.startsWith("image/")) {
+            (function (file) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    // Crear elemento de imagen para la previsualización
                     var img = document.createElement("img");
                     img.src = e.target.result;
                     img.className = "img-preview";
+
+                    var containerItem = document.createElement("div");
+                    containerItem.className = "img-preview-container";
                     containerItem.appendChild(img);
 
-                    var deleteButton = document.createElement("button");
-                    deleteButton.innerHTML = "x";
-                    deleteButton.className = "delete-button";
-                    deleteButton.onclick = function () {
-                        handleImageDelete(index);
-                    };
-                    containerItem.appendChild(deleteButton);
                     container.appendChild(containerItem);
                 };
-            })(file, i);
-            reader.readAsDataURL(file);
+
+                reader.readAsDataURL(file);
+            })(file);
         }
     }
 }
+
+
