a802705b209c77b2f03919324c3f90c789b780df
CristopherH11 <cristo1206hc@gmail.com>
2023-11-21 22:10:01 -0600

    Se realiza la funcionalidad para que se muestre la tabla con los establecimientos que presentan los productos de la lista.

--- source/LoCoMPro_LV/Pages/Lists/Details.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Lists/Details.cshtml.cs
@@ -7,16 +7,28 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LoCoMPro_LV.Data;
 using LoCoMPro_LV.Models;
+using LoCoMPro_LV.Pages.Records;
+using LoCoMPro_LV.Utils;
+using System.Data.SqlClient;
 
 namespace LoCoMPro_LV.Pages.Lists
 {
     public class DetailsModel : PageModel
     {
-        private readonly LoCoMPro_LV.Data.LoComproContext _context;
+        /// <summary>
+        /// Contexto de la base de datos de LoCoMPro.
+        /// </summary>
+        private readonly LoComproContext _context;
 
-        public DetailsModel(LoCoMPro_LV.Data.LoComproContext context)
+        /// <summary>
+        /// Se utiliza para acceder a las utilidades de la base de datos.
+        /// </summary>
+        private readonly DatabaseUtils _databaseUtils;
+
+        public DetailsModel(LoComproContext context, DatabaseUtils databaseUtils)
         {
             _context = context;
+            _databaseUtils = databaseUtils;
         }
 
         public List List { get; set; }
@@ -27,23 +39,67 @@ namespace LoCoMPro_LV.Pages.Lists
         [BindProperty(SupportsGet = true)]
         public string NameList { get; set; }
 
+        public IList<ListSearchResults> Results { get; set; } = new List<ListSearchResults>();
+
+
         public async Task<IActionResult> OnGetAsync()
         {
-            if (_context.List == null)
+            string connectionString = _databaseUtils.GetConnectionString();
+            string sqlQuery = "SELECT * FROM dbo.GetStoresWithProducts(@NameList)";
+            SqlParameter[] parameters = new SqlParameter[]
             {
-                return NotFound();
+                new SqlParameter("@NameList", User.Identity.Name)
+            };
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddRange(parameters);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string nameStore = reader.GetString(reader.GetOrdinal("NameStore"));
+                            double longitude = reader.GetDouble(reader.GetOrdinal("Longitude"));
+                            double latitude = reader.GetDouble(reader.GetOrdinal("Latitude"));
+                            int productCount = reader.GetInt32(reader.GetOrdinal("cantidad_productos"));
+
+                            ListSearchResults result = new ListSearchResults();
+
+                            result.productCount = productCount;
+
+                            result.Store = await _context.Stores
+                                .FirstOrDefaultAsync(m => m.Latitude == latitude && m.Longitude == longitude && m.NameStore == nameStore);
+
+                            var query = from record in _context.Records
+                                        join listed in _context.Listed
+                                        on record.NameProduct equals listed.NameProduct
+                                        where record.NameStore == nameStore
+                                        && record.Latitude == latitude
+                                        && record.Longitude == longitude
+                                        orderby record.RecordDate descending
+                                        group record by record.NameProduct into grouped
+                                        select grouped.First();
+
+                            result.Records = await query.ToListAsync();
+
+                            foreach(var record in result.Records)
+                            {
+                                result.totalPrice += record.Price;
+                            }
+
+                            Results.Add(result);
+                        }
+                    }
+                }
             }
 
-            var list = await _context.List.FirstOrDefaultAsync(m => m.NameList == User.Identity.Name);
+            Results = Results.OrderByDescending(r => r.productCount).ToList();
 
-            if (list == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                List = list;
-            }
             return Page();
         }
     }
---
+++ source/LoCoMPro_LV/Utils/ListSearchResults.cs
@@ -0,0 +1,12 @@
+﻿using LoCoMPro_LV.Models;
+
+namespace LoCoMPro_LV.Utils
+{
+    public class ListSearchResults
+    {
+        public Store Store { get; set; }
+        public int productCount { get; set; }
+        public double? totalPrice { get; set; } = 0;
+        public IList<Record> Records { get; set; }
+    }
+}
