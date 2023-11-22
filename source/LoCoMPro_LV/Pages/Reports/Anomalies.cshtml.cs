2f125306e9689b6739079d0cac9053e9cf52997a
JamesLezcano <73266308+JamesLezcano@users.noreply.github.com>
2023-11-22 17:01:11 -0600

    Se implenta funcionalidad de anomalias por precio

--- source/LoCoMPro_LV/Pages/Reports/Anomalies.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Reports/Anomalies.cshtml.cs
@@ -236,6 +236,7 @@ namespace LoCoMPro_LV.Pages.Reports
                     recordsGroupContainer.Add(record);
                 }
                 AnomaliesDate(recordsGroupContainer);
+                AnomaliesPrice(recordsGroupContainer);
                 recordsGroupContainer.Clear();
             }
         }
@@ -244,7 +245,7 @@ namespace LoCoMPro_LV.Pages.Reports
         {
             List<RecordStoreModel> selectedRecords = new List<RecordStoreModel>();
             var sortedRecords = recordsGroupContainer.OrderBy(r => r.Record.RecordDate).ToList();
-            int endIndex = (int)(sortedRecords.Count * 0.2);
+            int endIndex = (int)(sortedRecords.Count * 0.5);
             var selectedRecordsSubset = sortedRecords.Take(endIndex).ToList();
 
             selectedRecords.AddRange(selectedRecordsSubset.Where(r => r.Record.Hide == false));
@@ -255,7 +256,7 @@ namespace LoCoMPro_LV.Pages.Reports
                 {
                     NameGenerator = indice.Record.NameGenerator,
                     RecordDate = indice.Record.RecordDate,
-                    Type = "Date",
+                    Type = "Fecha",
                     Comment = "La fecha es muy antigua",
                     State = 0
                 };
@@ -271,21 +272,98 @@ namespace LoCoMPro_LV.Pages.Reports
         private async Task AnomaliesPrice(List<RecordStoreModel> recordsGroupContainer)
         {
             List<RecordStoreModel> selectedRecords = new List<RecordStoreModel>();
+            var sortedRecords = recordsGroupContainer.OrderBy(r => r.Record.Price).ToList();  // Ordena por precio
 
-            foreach (var indice in selectedRecords)
+            // Calcula Q2
+            int q2Index = CalculateQ2Index(sortedRecords.Count);
+
+            // Calcula Q1 y Q3 utilizando los índices de Q2
+            int q1Index = CalculateQ1Index(q2Index);
+            int q3Index = CalculateQ3Index(q2Index);
+            double? q1;
+            double? q3;
+
+            // Obtiene los valores reales de Q1, Q2 y Q3
+            if (q2Index % 2 == 0)
             {
-                Anomalie anomalie = new Anomalie
-                {
-                    NameGenerator = indice.Record.NameGenerator,
-                    RecordDate = indice.Record.RecordDate,
-                    Type = "Price",
-                    Comment = "El precio es anomalo",
-                    State = 0
-                };
-                _context.Anomalies.Add(anomalie);
-                await _context.SaveChangesAsync();
+                q1 = ((sortedRecords[q1Index].Record.Price) + (sortedRecords[q1Index + 1].Record.Price)) / 2;
+                q3 = ((sortedRecords[q3Index].Record.Price) + (sortedRecords[q3Index - 1].Record.Price)) / 2;
             }
+            else
+            {
+                q1 = sortedRecords[q1Index].Record.Price;
+                q3 = sortedRecords[q3Index].Record.Price;
+            }
+
+            // Calcula el RIC
+            double? ric = q3 - q1;
+
+            // Define el umbral (puedes ajustar este valor según tus necesidades)
+            double umbral = 1.5;
+
+            // Establece el rango para valores no atípicos
+            double? lowerBound = q1 - umbral * ric;
+            double? upperBound = q3 + umbral * ric;
+
+            // Identifica los valores atípicos y agrégales a selectedRecords
+            foreach (var indice in sortedRecords)
+            {
+                if (indice.Record.Price < lowerBound || indice.Record.Price > upperBound)
+                {
+                    Anomalie anomalie = new Anomalie
+                    {
+                        NameGenerator = indice.Record.NameGenerator,
+                        RecordDate = indice.Record.RecordDate,
+                        Type = "Precio",
+                        Comment = "El precio es anómalo",
+                        State = 0
+                    };
+                    _context.Anomalies.Add(anomalie);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            // Limpia la lista de registros seleccionados
             selectedRecords.Clear();
+        }
+
+        private int CalculateQ2Index(int recordCount)
+        {
+            // La mediana siempre es el valor en el centro o el promedio de los dos valores centrales
+            if (recordCount % 2 == 0)
+            {
+                // Si la cantidad de datos es par, la mediana es el índice del valor en el centro
+                return recordCount / 2;
+            }
+            else
+            {
+                // Si la cantidad de datos es impar, la mediana es el índice del valor en el centro
+                return ((recordCount - 1) / 2) + 1;
+            }
+        }
+
+        private int CalculateQ1Index(int q2)
+        {
+            if (q2 % 2 == 0)
+            {
+                return (int)((q2 / 2) - 1);
+            }
+            else
+            {
+                return (int)(((q2 - 1) / 2));
+            }
+        }
+
+        private int CalculateQ3Index(int q2)
+        {
+            if (q2 % 2 == 0)
+            {
+                return (int)((q2 + (q2 / 2)) - 1);
+            }
+            else
+            {
+                return (int)(q2 + ((q2 - 1) / 2));
+            }
         }
     }
 }
