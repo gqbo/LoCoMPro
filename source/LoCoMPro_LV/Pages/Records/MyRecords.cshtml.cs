99cd5991153e75f7160dcbec34d44c127dd6759f
Yordi Robles Siles <yordi.robles@ucr.ac.cr>
2023-11-25 10:21:56 -0600

    Se modulariza el método OnPost.

--- source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
+++ source/LoCoMPro_LV/Pages/Records/MyRecords.cshtml.cs
@@ -27,9 +27,6 @@ namespace LoCoMPro_LV.Pages.Records
         public List<Record> Records { get; set; }
 
         [BindProperty]
-        public string AuthenticatedUserName { get; set; }
-
-        [BindProperty]
         public DateTime RecordDate { get; set; }
 
         /// <summary> 
@@ -37,47 +34,62 @@ namespace LoCoMPro_LV.Pages.Records
         /// </summary>
         public async Task OnGetAsync()
         {
-            AuthenticatedUserName = User.Identity.Name;
             Records = await _context.Records
                 .Include(r => r.Store)
-                .Where(r => r.NameGenerator == AuthenticatedUserName)
+                .Where(r => r.NameGenerator == User.Identity.Name)
                 .ToListAsync();
         }
 
         /// <summary>
-        /// Este metodo se utiliza cuando se va a eliminar un registro realizado por un usuario registrado.
+        /// Este metodo se utiliza cuando se elimina un registro realizado por un usuario registrado.
         /// </summary>
-        public async Task<IActionResult> OnPostEliminarRegistro()
+        public async Task<IActionResult> OnPostDeleteRecord()
         {
             var record = await _context.Records
                 .FirstOrDefaultAsync(r => r.NameGenerator == User.Identity.Name && r.RecordDate == RecordDate);
             if (record != null)
             {
-                var reports = await _context.Reports
-                    .Where(r => r.NameGenerator == User.Identity.Name && r.RecordDate == RecordDate)
-                    .ToListAsync();
-                if (reports != null)
-                {
-                    foreach (var report in reports)
-                    {
-                        _context.Reports.Remove(report);
-                    }
-                }
-                var valorations = await _context.Valorations
-                    .Where(v => v.NameGenerator == User.Identity.Name && v.RecordDate == RecordDate)
-                    .ToListAsync();
-
-                if (valorations != null)
-                {
-                    foreach (var valoration in valorations)
-                    {
-                        _context.Valorations.Remove(valoration);
-                    }
-                }
+                await DeleteReports();
+                await DeleteValorations();
                 _context.Records.Remove(record);
             }
             await _context.SaveChangesAsync();
             return RedirectToPage("./MyRecords");
+        }
+
+        /// <summary>
+        /// Elimina los reportes relacionados al registro a eliminar.
+        /// </summary>
+        private async Task DeleteReports()
+        {
+            var reports = await _context.Reports
+                .Where(r => r.NameGenerator == User.Identity.Name && r.RecordDate == RecordDate)
+                .ToListAsync();
+            if (reports != null)
+            {
+                foreach (var report in reports)
+                {
+                    _context.Reports.Remove(report);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Elimina las valoraciones relacionadas al registro a eliminar.
+        /// </summary>
+        private async Task DeleteValorations()
+        {
+            var valorations = await _context.Valorations
+                .Where(v => v.NameGenerator == User.Identity.Name && v.RecordDate == RecordDate)
+                .ToListAsync();
+
+            if (valorations != null)
+            {
+                foreach (var valoration in valorations)
+                {
+                    _context.Valorations.Remove(valoration);
+                }
+            }
         }
     }
 }
